5c5788b95a0c30272f3f61d9f068b16bb19d950e
Hugh Guiney <hugh@hughguiney.com>
2017-04-05 00:39:14 -0400

    Minified build test

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,29 @@ var htmlmin = require('gulp-htmlmin');
 // var uglifyInline = require('gulp-uglify-inline');
 var minifyInline = require('gulp-minify-inline');
 
+gulp.task('copy:server', function () {
+  return gulp.src(['.htaccess', 'favicon.*', 'apple-touch-*', 'index.html', 'CNAME'])
+    .pipe(gulp.dest('build/'));
+});
+
+// gulp.task('copy:shared-styles', function () {
+//   return gulp.src('src/shared-styles/**/*')
+//     .pipe(gulp.dest('build/src/shared-styles/'));
+// });
+
+gulp.task('copy:images', function () {
+  return gulp.src('src/images/**/*')
+    .pipe(gulp.dest('build/src/images/'));
+});
+
+gulp.task('copy:fonts', function () {
+  return gulp.src('src/fonts/**/*')
+    .pipe(gulp.dest('build/src/fonts/'));
+});
+
+// 'copy:shared-styles'
+gulp.task('copy', ['copy:server', 'copy:images', 'copy:fonts']);
+
 gulp.task('vulcanize', function() {
   return gulp.src('index.html')
     .pipe(vulcanize())
