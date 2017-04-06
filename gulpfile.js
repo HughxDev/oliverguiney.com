f1f34f17f3fa1acf0183c4199eb332dac21272c4
Hugh Guiney <hugh@hughguiney.com>
2017-04-06 08:29:18 -0400

    Trying to fix blank page in non-Chrome Desktop

--- gulpfile.js
+++ gulpfile.js
@@ -159,9 +159,9 @@ gulp.task('minify:images', function(){
     .pipe(gulp.dest('build/images'));
 });
 
-gulp.task('minify', ['minify:inline']);
+gulp.task('minify', ['minify:html']);
 
 /* Default */
 // Beceause each meta-task relies on the last, we only have to call the latter task in order to call all previous tasks up the chain
-// gulp.task('default', ['minify']);
-gulp.task('default', ['rewrite']);
+gulp.task('default', ['minify']);
+// gulp.task('default', ['rewrite']);
