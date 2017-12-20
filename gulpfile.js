788abfe4b84ae4951b090be9547c93ed2e463e24
Hugh Guiney <hugh@hughguiney.com>
2017-12-20 05:43:50 -0500

    Include touch-icon-192x192.png in build output

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('clean', function () {
 
 /* Copy */
 gulp.task('copy:server', ['clean'], function () {
-  return gulp.src(['.htaccess', 'favicon.*', 'apple-touch-*', 'index.html', 'CNAME'])
+  return gulp.src(['.htaccess', 'favicon.*', '*touch-icon*', 'index.html', 'CNAME'])
     .pipe(gulp.dest('build/'));
 });
 
